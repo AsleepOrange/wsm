3bde0e08d72c312f7a249dcba15612f6cc6acfe9
Orange <99988155+AsleepOrange@users.noreply.github.com>
2025-05-07 22:36:09 -0500

    added darkening effect
    so the paint looks dirty i guess 😱

--- src/processor.rs
+++ src/processor.rs
@@ -5,6 +5,7 @@ use rand::Rng;
 use std::time::{SystemTime, UNIX_EPOCH};
 use std::{vec};
 
+pub const RESOLUTION_DIVISION: f64 = 1000.0; // how many pixels is needed to scale the image, e.g. image size = 2000 and RES_DIV is 100.0 would result in a noise scale of 20.0
 pub const POINT_FREQUENCY: u16 = 10; // for every x pixels, spawn a point
 pub const POINT_SIZE_RANGE: [u8; 2] = [1, 2]; // how big points can be (in pixels)
 pub const NOISE_DIVISION: f64 = 20.0;
@@ -17,6 +18,7 @@ pub struct Processor {
   rng: ThreadRng,
   noise: Simplex,
   pub points: Vec<PointData>,
+  pub noise_res: f64,
 }
 
 impl Processor {
@@ -24,6 +26,9 @@ impl Processor {
   {
     let mut rng = rand::rng();
     let mut points = vec![];
+    let mut noise_res = image_width as f64 / RESOLUTION_DIVISION;
+    if image_height as f64 / RESOLUTION_DIVISION > noise_res {noise_res = image_height as f64 / RESOLUTION_DIVISION}
+
     for _ in 0..(image_width*image_height/POINT_FREQUENCY as u64) {
       let size = rng.random_range(POINT_SIZE_RANGE[0]..POINT_SIZE_RANGE[1]);
       let position_x = rng.random_range(0..image_width);
@@ -37,6 +42,7 @@ impl Processor {
     
     Processor {
       rng: rng,
+      noise_res: noise_res,
       noise: Simplex::new(SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs() as u32), // todo make this random
       points: points,
     }
@@ -52,11 +58,20 @@ impl Processor {
   pub fn process_pixel(self: &Processor, position: [u64; 2], pixel: &Rgba<u8>) -> Rgba<u8> {
     // do some noise on it for distributed wearing
     let mut output_pixel: Rgba<u8> = pixel.clone();
-    output_pixel.0[3] = (output_pixel.0[3] as f64 * ((self.noise.get([position[0] as f64 / NOISE_DIVISION, position[1] as f64 / NOISE_DIVISION]).abs()/3.0) +
-      (self.noise.get([position[0] as f64 / NOISE_DIVISION * 2.0, position[1] as f64 / NOISE_DIVISION * 2.0]).abs()/3.0) + // octave 2 
-      (self.noise.get([position[0] as f64 / NOISE_DIVISION * 4.0, position[1] as f64 / NOISE_DIVISION * 4.0]).abs()/3.0)) // octave 3
+    output_pixel.0[3] = (output_pixel.0[3] as f64 * ((self.noise.get([position[0] as f64 / NOISE_DIVISION * self.noise_res, position[1] as f64 / NOISE_DIVISION * self.noise_res]).abs()/3.0) +
+      (self.noise.get([position[0] as f64 / NOISE_DIVISION * 2.0 * self.noise_res, position[1] as f64 / NOISE_DIVISION * 2.0 * self.noise_res]).abs()/3.0) + // octave 2 
+      (self.noise.get([position[0] as f64 / NOISE_DIVISION * 4.0 * self.noise_res, position[1] as f64 / NOISE_DIVISION * 4.0 * self.noise_res]).abs()/3.0)) // octave 3
     ) as u8;
 
+    // make some pixels darker (this is literally just the noise fn but in a diff position)
+    let noisefn_calculated = ((self.noise.get([position[0] as f64 / NOISE_DIVISION * self.noise_res, position[1] as f64 / NOISE_DIVISION * self.noise_res, 5.0, 5.0]).abs()/3.0) +
+      (self.noise.get([position[0] as f64 / NOISE_DIVISION * 2.0 * self.noise_res, position[1] as f64 / NOISE_DIVISION * 2.0 * self.noise_res, 5.0, 5.0]).abs()/3.0) + // octave 2 
+      (self.noise.get([position[0] as f64 / NOISE_DIVISION * 4.0 * self.noise_res, position[1] as f64 / NOISE_DIVISION * 4.0 * self.noise_res, 5.0, 5.0]).abs()/3.0)); // octave 3
+
+    output_pixel.0[0] = (output_pixel.0[0] as f64 * 0.5 + noisefn_calculated * 0.5) as u8;
+    output_pixel.0[1] = (output_pixel.0[1] as f64 * 0.5 + noisefn_calculated * 0.5) as u8;
+    output_pixel.0[2] = (output_pixel.0[2] as f64 * 0.5 + noisefn_calculated * 0.5) as u8;
+    
     for point in &self.points {
       let dist = Processor::get_magnitude([point.position[0] as f64, point.position[1] as f64], [position[0] as f64, position[1] as f64]);
       if dist < point.size as f64 {
@@ -72,9 +87,9 @@ impl Processor {
         // g
         let mut output_pixel: Rgba<u8> = pixel.clone();
         output_pixel.0[1] = (output_pixel.0[1] as f64 + 
-          255.0 * ((self.noise.get([position[0] as f64 / NOISE_DIVISION, position[1] as f64 / NOISE_DIVISION]).abs()/3.0) +
-          (self.noise.get([position[0] as f64 / NOISE_DIVISION * 2.0, position[1] as f64 / NOISE_DIVISION * 2.0]).abs()/3.0) + // octave 2 
-          (self.noise.get([position[0] as f64 / NOISE_DIVISION * 4.0, position[1] as f64 / NOISE_DIVISION * 4.0]).abs()/3.0)) // octave 3
+          255.0 * ((self.noise.get([position[0] as f64 / NOISE_DIVISION * self.noise_res, position[1] as f64 / NOISE_DIVISION * self.noise_res]).abs()/3.0) +
+          (self.noise.get([position[0] as f64 / NOISE_DIVISION * 2.0 * self.noise_res, position[1] as f64 / NOISE_DIVISION * 2.0 * self.noise_res]).abs()/3.0) + // octave 2 
+          (self.noise.get([position[0] as f64 / NOISE_DIVISION * 4.0 * self.noise_res, position[1] as f64 / NOISE_DIVISION * 4.0 * self.noise_res]).abs()/3.0)) // octave 3
         ) as u8;
         println!("{}", output_pixel.0[1]);
         
